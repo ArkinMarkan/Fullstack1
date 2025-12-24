fede9566b2df8aeb16ece4e30c349b0efe14d95c
ArkinMarkan <68769267+ArkinMarkan@users.noreply.github.com>
2025-12-24 23:27:46 +0530

    Update and rename AuthController.java to TicketBookingDto.java

---
+++ TicketBookingDto.java
@@ -0,0 +1,96 @@
+package com.moviebookingapp.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Max;
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+/**
+ * DTO for ticket booking request
+ */
+public class TicketBookingDto {
+    
+    @NotBlank(message = "Movie name is mandatory")
+    @JsonProperty("movieName")
+    @JsonAlias({"movie_name"})
+    private String movieName;
+    
+    @NotBlank(message = "Theatre name is mandatory")
+    @JsonProperty("theatreName")
+    @JsonAlias({"theatre_name"})
+    private String theatreName;
+    
+    @NotNull(message = "Number of tickets is mandatory")
+    @Min(value = 1, message = "Number of tickets must be at least 1")
+    @Max(value = 10, message = "Maximum 10 tickets can be booked at once")
+    @JsonProperty("numberOfTickets")
+    @JsonAlias({"number_of_tickets"})
+    private Integer numberOfTickets;
+    
+    // Seat numbers optional for tests; service can validate when provided
+    @JsonProperty("seatNumbers")
+    @JsonAlias({"seat_numbers"})
+    private List<String> seatNumbers;
+    
+    // Constructors
+    public TicketBookingDto() {}
+    
+    public TicketBookingDto(String movieName, String theatreName, 
+                           Integer numberOfTickets, List<String> seatNumbers) {
+        this.movieName = movieName;
+        this.theatreName = theatreName;
+        this.numberOfTickets = numberOfTickets;
+        this.seatNumbers = seatNumbers;
+    }
+    
+    // Getters and Setters
+    public String getMovieName() {
+        return movieName;
+    }
+    
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+    
+    public String getTheatreName() {
+        return theatreName;
+    }
+    
+    public void setTheatreName(String theatreName) {
+        this.theatreName = theatreName;
+    }
+    
+    public Integer getNumberOfTickets() {
+        return numberOfTickets;
+    }
+    
+    public void setNumberOfTickets(Integer numberOfTickets) {
+        this.numberOfTickets = numberOfTickets;
+    }
+    
+    public List<String> getSeatNumbers() {
+        return seatNumbers;
+    }
+    
+    public void setSeatNumbers(List<String> seatNumbers) {
+        this.seatNumbers = seatNumbers;
+    }
+    
+    // Validation methods
+    public boolean isValidSeatCount() {
+        return seatNumbers == null || numberOfTickets == null || seatNumbers.size() == numberOfTickets;
+    }
+    
+    @Override
+    public String toString() {
+        return "TicketBookingDto{" +
+                "movieName='" + movieName + '\'' +
+                ", theatreName='" + theatreName + '\'' +
+                ", numberOfTickets=" + numberOfTickets +
+                ", seatNumbers=" + seatNumbers +
+                '}';
+    }
+}
